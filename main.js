76496040af133d36af93ea7099a57789b8de5f49
Gerard Causse <grod.causse@gmail.com>
2025-05-17 17:39:24 -0400

    Complete BrowserWindow: Parent & Child Windows lesson

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require("electron");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow, secondaryWindow;
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
@@ -20,11 +20,37 @@ function createWindow() {
     // Don't show window (immediately, we show later on)
     // show: false,
     // Useful to give better illusion of rapid load
-    backgroundColor: "#2B2E3B",
+    // backgroundColor: "#2B2E3B",
+  });
+
+  secondaryWindow = new BrowserWindow({
+    width: 600,
+    height: 300,
+    webPreferences: {
+      // --- !! IMPORTANT !! ---
+      // Disable 'contextIsolation' to allow 'nodeIntegration'
+      // 'contextIsolation' defaults to "true" as from Electron v12
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+    // Make this a child
+    parent: mainWindow,
+    // Make this a modal
+    modal: true,
+    show: false,
   });
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile("index.html");
+  secondaryWindow.loadFile("second.html");
+
+  setTimeout(() => {
+    secondaryWindow.show();
+    setTimeout(() => {
+      secondaryWindow.close();
+      secondaryWindow = null;
+    }, 3000);
+  }, 3000);
   // Remember, electron uses a Chromium browser, so we can load web pages
   // mainWindow.loadURL("https://google.com");
 
@@ -40,6 +66,9 @@ function createWindow() {
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
+  // secondaryWindow.on("closed", () => {
+  //   secondaryWindow = null;
+  // });
 }
 
 // Electron `app` is ready
